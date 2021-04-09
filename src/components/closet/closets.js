8e8bb3f57c7c55b78155321029b720136bc9f063
olamide oladinni <olajide1987@gmail.com>
2021-04-09 02:13:19 -0500

    wip

--- src/components/closet/closets.js
+++ src/components/closet/closets.js
@@ -1,10 +1,16 @@
+// I want to print closet cards for all three categories
+// on the card it should show the name of the closet, image, and button to view whats inside
+// I will need to import product into the closet component
+// i will need to pass the props for product in the closet component
+// I need to write a function that upon clicking the view detail button it will grab all the products
+// Based on closet id and display them as product view
 import React from 'react';
 
 import closetData from '../../data/closetData';
 
 class Closets extends React.Component {
   state = {
-    closets: [],
+    closets: {},
   }
 
   getClosetData = () => {
@@ -13,18 +19,27 @@ class Closets extends React.Component {
       .catch((err) => console.error(err));
   }
 
+  // viewClosetEvent = (e) => {
+  //   e.preventDefault();
+  //   const {} =
+  // }
+
   componentDidMount() {
     this.getClosetData();
   }
 
   render() {
-    // const { closets } = this.state;
-    // const closet = closets.map((singleCloset) => <Closet key={singleClosets.id} closet={closet} getClosetByUser={this.getClosetByUser} />);
+    // const closet = this.state;
     return (
-      <div className="text-center">
-        <h1 className="text-center mt-3 headline">Closet</h1>
-        <div className="closet-container">
-          {/* {closet} */}
+      <div className="card" >
+        <div className="card-body">
+          <h5 className="card-title">Shoes</h5>
+          {/* <img className="card-img" src={garageCategory.imageUrl} alt={garageCategory.categoryName}/> */}
+          <div className='btn-group' role='group'>
+            <div className="btn-group" role="group">
+            <button className="btn btn-success" ><i className="far fa-eye"></i></button>
+            </div>
+          </div>
         </div>
       </div>
     );
